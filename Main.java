61da3607c49e8f43d79298c7ed78f683b8c4ba16
Benlin Gan <>
2021-11-19 01:18:39 +0000

    restart

--- Main.java
+++ Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
-    Frame f = new Frame("level1.txt");
-    Prompt p = new Prompt();
+    Frame frame = new Frame("level1.txt");
+    Prompt prompt = new Prompt();
     while (true){
       System.out.print("\033[H\033[2J");  
       System.out.flush(); 
       System.out.println("SOKOBAN - Console Edition"); 
-      System.out.println(f);
-      System.out.println(p);
+      System.out.println(frame);
+      System.out.println(prompt);
       String command = stdin.next();
-      //System.out.print(command.trim());
-      f.prepareNextFrame(command.trim());
+      if(command.trim().equals("r")){
+        frame = new Frame("level1.txt");
+      }
+      frame.prepareNextFrame(command.trim());
     }
     //stdin.close();
   }
