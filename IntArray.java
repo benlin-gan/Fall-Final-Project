dd3b54f606f8de44d9bba7dc0ebd658617f352bc
Benlin Gan <94497428+benlin-gan@users.noreply.github.com>
2021-11-22 16:27:56 +0000

    Faster Array

--- IntArray.java
+++ IntArray.java
@@ -1,30 +1,24 @@
 public class IntArray{
   //wrapper around a string of a list of integers with O(N) access time;
   private String data;
-  public static final String DELIMETER = ","; 
   public IntArray(){
     this.data = "";
   }
   public void push(int input){
-    this.data += DELIMETER + input;
+    this.data += getRepresentation(input);
   }
   public int at(int index){
-    String out = "";
-    int word = -1;
-    for(int i = 0; i < this.data.length(); i++){
-      String curr = this.data.substring(i, i+1);
-      if(curr.equals(DELIMETER)){
-        if(index == word++){
-          return Integer.parseInt(out);
-        }
-        out = "";
-      }else{
-        out += curr;
-      }
+    return Integer.parseInt(data.substring(index * 10, index * 10 + 10));
+  }
+  private static String getRepresentation(int input){
+    String prefix = "+";
+    if(input < 0){
+      prefix = "-";
     }
-    if (out.length() > 0) {
-      return Integer.parseInt(out);
+    String init = "" + Math.abs(input);
+    while(init.length() < 9){
+      init = "0" + init;
     }
-    return 42; //returns if index out of bounds;
+    return prefix + init;
   }
 }
