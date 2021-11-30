f305f53bc9fbb67d74267bca3dfea5b505bc6119
BenlinGan <>
2021-11-30 18:18:53 +0000

    this.location

--- LevelPack.java
+++ LevelPack.java
@@ -5,7 +5,7 @@ public class LevelPack{
   private String title;
   private String description;
   private String copyright;
-  private int location;
+  private int location; //argument passed to indexOf that restricts search space. Ideally, we don't ever need to backtrack, leading to every character in the XML file being read only once, leading to amortized O(N) access time;
   public LevelPack(String path){
     //File IO code copied form Consumer Review Lab
     try{
@@ -18,10 +18,10 @@ public class LevelPack{
     }catch(Exception e){
       System.out.println("Error!");
     }
+    this.location = 0;
     this.title = simpleTag("Title");
     this.description = simpleTag("Description").strip();
-    this.copyright = LevelPack.parseVariable(this.baseTag("LevelCollection"), "Copyright");
-    this.location = this.raw.indexOf("<Level");
+    this.copyright = parseVariable(this.baseTag("LevelCollection"), "Copyright");
   }
   @Override
   public String toString() {
@@ -36,17 +36,21 @@ public class LevelPack{
     //returns the first subsection of the data which is wrapped in the tags specified by tagName;
     String openingTag = "<" + tagName + "";
     String closingTag = "</" + tagName + ">";
-    return this.raw.substring(this.raw.indexOf(openingTag) + openingTag.length(), this.raw.indexOf(closingTag));
+    int begin = this.raw.indexOf(openingTag, this.location) + openingTag.length();
+    this.location = begin; 
+    int end = this.raw.indexOf(closingTag, this.location); 
+    return this.raw.substring(begin, end);
   }
   public String simpleTag(String tagName){
     //A "simpleTag" is a tag with no variables
     //ie: <tagName>[CONTENT]</tagName>
     return this.baseTag(tagName).substring(1);
   }
-  public static String parseVariable(String data, String variableName){
+  public String parseVariable(String data, String variableName){
     int begin = data.indexOf(variableName) + variableName.length() + 2;
     //one character for the equal sign, one character for the quote;
     int end = data.indexOf("\"", begin);
+    this.location = end;
     return data.substring(begin, end);
   }
 } 
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main{
   public static void main(String[] args){
     LevelPack packOne = new LevelPack("696.slc");
     System.out.println(packOne);
-    System.out.println(packOne.getLevel());
+    System.out.println("\n" + packOne.getLevel());
     //Game g = new Game(1);
     //g.loop();
   }
