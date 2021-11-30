4474f70e40318364b9d974679142dcecb87d5ea7
BenlinGan <>
2021-11-30 18:06:14 +0000

    non static

--- LevelPack.java
+++ LevelPack.java
@@ -18,31 +18,30 @@ public class LevelPack{
     }catch(Exception e){
       System.out.println("Error!");
     }
-    this.title = LevelPack.simpleTag(this.raw, "Title");
-    this.description = LevelPack.simpleTag(this.raw, "Description").strip();
-    this.copyright = LevelPack.parseVariable(LevelPack.baseTag(this.raw, "LevelCollection"), "Copyright");
+    this.title = simpleTag("Title");
+    this.description = simpleTag("Description").strip();
+    this.copyright = LevelPack.parseVariable(this.baseTag("LevelCollection"), "Copyright");
     this.location = this.raw.indexOf("<Level");
   }
   @Override
   public String toString() {
-
     return "Level Pack: " + this.title + 
            "\nDescription: " + this.description +
            "\nCopyright: " + this.copyright;
   }
   public String getLevel(){
-    return LevelPack.baseTag(this.raw, "Level");
+    return this.baseTag("Level");
   }
-  public static String baseTag(String data, String tagName){
+  public String baseTag(String tagName){
     //returns the first subsection of the data which is wrapped in the tags specified by tagName;
     String openingTag = "<" + tagName + "";
     String closingTag = "</" + tagName + ">";
-    return data.substring(data.indexOf(openingTag) + openingTag.length(), data.indexOf(closingTag));
+    return this.raw.substring(this.raw.indexOf(openingTag) + openingTag.length(), this.raw.indexOf(closingTag));
   }
-  public static String simpleTag(String data, String tagName){
+  public String simpleTag(String tagName){
     //A "simpleTag" is a tag with no variables
     //ie: <tagName>[CONTENT]</tagName>
-    return baseTag(data, tagName).substring(1);
+    return this.baseTag(tagName).substring(1);
   }
   public static String parseVariable(String data, String variableName){
     int begin = data.indexOf(variableName) + variableName.length() + 2;
