a370d296dea4d71cd015e89fbb9ceb1569f6ad04
Benlin Gan <benlingan@gmail.com>
2021-12-07 07:41:14 -0800

    fixed typo

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
 class Main{
-  //Represents the nain nenu
+  //Represents the main menu
   public static void main(String[] args){
     Scanner stdin = new Scanner(System.in);
     String levelNames = "";
